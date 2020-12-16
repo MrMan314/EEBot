92242a97519b68682a3b3f8a7d606cd49d0b504f
MrMan999 <oofisoofoofoof@gmail.com>
2020-12-16 10:17:54 -0500

    Errors don't crash now

--- bot/bot.py
+++ bot/bot.py
@@ -30,11 +30,7 @@ async def on_member_join(member):
 
 @client.event
 async def on_command_error(ctx, error):
-    if isinstance(error, CommandNotFound):
-        await ctx.send("Error: " + str(error))
-        return
-    await ctx.send("Error: " + str(error))
-    raise error
+    await ctx.send(str(error))
 
 
 @client.command()
